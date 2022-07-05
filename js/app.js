31ebfb43b14913eb56894074f98d5220d8a570bd
GuanMingChee <s530506@nwmissouri.edu>
2022-07-05 00:36:23 -0400

    post api done

--- js/app.js
+++ js/app.js
@@ -1,30 +1,95 @@
+const get_orders="http://localhost:8080/api/order/getorders";
+const post_order="http://localhost:8080/api/order/postorder";
+const get_order="http://localhost:8080/api/order/getorderdetail";
 
-function cal() {
-	var a = document.getElementById("p1qop").value;
-	var b = document.getElementById("p2qop").value;
-	var c = document.getElementById("p3qop").value;
-  var order_list = {};
-  var product_list = {};
-  product_list["Hp Laptop"] = 1999;
-  product_list["Mac"] = 2500;
-  product_list["Dell Laptop"] = 1500;
-  if (a > 0) {
-    order_list["Hp Laptop"] = a;
+//post
+function send(){
+  const form = document.getElementById("form1");
+  if(form){
+  form.addEventListener("submit", function(e){
+    e.preventDefault();
+      // Create payload as new FormData object:
+      const payload = new FormData(form);
+      console.log(payload);
+      const data = {};
+      payload.forEach((value, key) => (data[key] = value));
+      // Post the payload using Fetch:
+      console.log(data);
+      fetch('http://localhost:8080/api/order/postorder', {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      // body: data,
+      body: JSON.stringify(data),
+      referrerPolicy: 'no-referrer',
+      mode: 'cors'
+      }).then(res => console.log(res));
+      //.then(data => console.log(data))
+  });}
+  else{
+    console.log("error");
   }
-  if (b > 0) {
-    order_list["Mac"] = b;
-  }
-  if (c > 0) {
-    order_list["Dell Laptop"] = c;
-  }
-  var total =
-    a * product_list["Hp Laptop"] +
-    b * product_list["Mac"] +
-    c * product_list["Dell Laptop"];
-  document.getElementById("display").innerHTML = "Total price: " + total;
-  console.log(total);
-  return [total, order_list];
 }
+//https://github.com/mdeveloper20/javascript-employee-project/blob/master/controller.js
+const api_getOrders = () => {
+  return fetch(get_orders);
+}
+
+const api_getOrder = (id) => {
+  return fetch(get_order);
+}
+
+const api_postOrder = (jsonData) => {
+  const headers = new Headers();
+
+  return fetch(post_order, {
+      headers,
+      method: 'POST',
+      body: JSON.stringify(jsonData)
+  });
+}
+
+window.onload = () => {
+  const table = document.querySelector('#ordersTable').getElementsByTagName('tbody')[0];
+  const searchButton = document.querySelector('#searchButton');
+  const searchQuery = document.querySelector('#searchQuery');
+  const addForm = document.querySelector('#addForm');
+  //const spinner = document.querySelector('#spinner');
+  //const newItemId = document.querySelector('#newItemId');
+
+
+  const addNewRow = (c1, c2, c3, c4, c5) => {
+      const row = table.insertRow();
+      const cell1 = row.insertCell(0);
+      const cell2 = row.insertCell(1);
+      const cell3 = row.insertCell(2);
+      const cell4 = row.insertCell(3);
+      const cell5 = row.insertCell(4);
+
+
+      cell1.innerHTML = c1;
+      cell2.innerHTML = c2;
+      cell3.innerHTML = c3;
+      cell4.innerHTML = c4;
+      cell5.innerHTML = c5;
+
+  }
+
+  const loadAll = () => {
+      api_getOrders().then(res => res.json()).then(jsonData => {
+          jsonData.data.forEach(order => {
+              addNewRow(order.id, order.name, order.email, order.order_product, order.order_price)
+
+          });
+
+
+      })
+  }
+}
+
+
 //test
 function sub() {
   fetch("http://localhost:8080/api/order/getorders", {
@@ -43,37 +108,6 @@ async function funcName() {
   const response = await fetch(uel);
   var data = await response.json();
 }
-
-function send(){
-const form = document.getElementById("form1");
-if(form){
-form.addEventListener("submit", function(e){
-  e.preventDefault();
-    // Create payload as new FormData object:
-    const payload = new FormData(form);
-    console.log(payload);
-    const data = {};
-    payload.forEach((value, key) => (data[key] = value));
-    // Post the payload using Fetch:
-    console.log(data);
-    fetch('http://localhost:8080/api/order/postorder', {
-    method: 'POST',
-    credentials: 'same-origin',
-    headers:{
-      'Content-Type': 'application/json'
-    },
-    // body: data,
-    body: JSON.stringify(data),
-    referrerPolicy: 'no-referrer',
-    mode: 'cors'
-    }).then(res => console.log(res));
-    //.then(data => console.log(data))
-});}
-else{
-  console.log("gg");
-}
-}
-
 
 async function getapi() {
   var url = "http://localhost:8080/api/order/getorders";
@@ -99,7 +133,11 @@ function submitBTN(){
 	var data={"order_price":cal_list[0],"order_list":cal_list[1]};
 	postapi(post_url,data);
 }
-
+/**
+ * 
+ * @param {*} url 
+ * @param {*} data 
+ 
 async function postapi(url = '', data = {}) {
 	// Default options are marked with *
 	const response = await fetch(url, {
@@ -116,4 +154,4 @@ async function postapi(url = '', data = {}) {
 	  body: JSON.stringify(data) // body data type must match "Content-Type" header
 	});
 	return response.json(); // parses JSON response into native JavaScript objects
-  }
+}*/
